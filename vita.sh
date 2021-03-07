1c375168f0eeb770227aa9a76d7a90c2f7b1c788
Andy Nguyen <theofficialflow1996@gmail.com>
2021-03-07 19:12:07 +0100

    Initial vita port.

---
+++ vita.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+set -e
+
+GENERAL="\
+   --enable-cross-compile \
+   --extra-libs="-lgcc" \
+   --arch=arm \
+   --cc=arm-vita-eabi-gcc \
+   --cross-prefix=arm-vita-eabi- \
+   --nm=arm-vita-eabi-nm"
+
+MODULES="\
+   --disable-avdevice \
+   --disable-filters \
+   --disable-programs \
+   --disable-network \
+   --disable-avfilter \
+   --disable-postproc \
+   --disable-encoders \
+   --disable-protocols \
+   --disable-hwaccels \
+   --disable-doc"
+
+VIDEO_DECODERS="\
+   --enable-decoder=h264 \
+   --enable-decoder=mpeg4 \
+   --enable-decoder=mpeg2video \
+   --enable-decoder=mjpeg \
+   --enable-decoder=mjpegb"
+
+AUDIO_DECODERS="\
+    --enable-decoder=aac \
+    --enable-decoder=aac_latm \
+    --enable-decoder=atrac3 \
+    --enable-decoder=atrac3p \
+    --enable-decoder=mp3 \
+    --enable-decoder=pcm_s16le \
+    --enable-decoder=pcm_s8"
+  
+DEMUXERS="\
+    --enable-demuxer=h264 \
+    --enable-demuxer=m4v \
+    --enable-demuxer=mpegvideo \
+    --enable-demuxer=mpegps \
+    --enable-demuxer=mp3 \
+    --enable-demuxer=avi \
+    --enable-demuxer=aac \
+    --enable-demuxer=pmp \
+    --enable-demuxer=oma \
+    --enable-demuxer=pcm_s16le \
+    --enable-demuxer=pcm_s8 \
+    --enable-demuxer=wav"
+
+VIDEO_ENCODERS="\
+	  --enable-encoder=huffyuv \
+	  --enable-encoder=ffv1 \
+	  --enable-encoder=mjpeg"
+
+AUDIO_ENCODERS="\
+	  --enable-encoder=pcm_s16le"
+
+MUXERS="\
+  	  --enable-muxer=avi"
+
+
+PARSERS="\
+    --enable-parser=h264 \
+    --enable-parser=mpeg4video \
+    --enable-parser=mpegaudio \
+    --enable-parser=mpegvideo \
+    --enable-parser=aac \
+    --enable-parser=aac_latm"
+
+
+function build_Vita
+{
+./configure --target-os=none \
+    --prefix=./vita \
+    ${GENERAL} \
+    --extra-cflags="-Wl,-q -O2 -ftree-vectorize -fno-short-enums -mfloat-abi=hard" \
+		--disable-neon \
+    --disable-shared \
+    --enable-static \
+    --enable-zlib \
+    --disable-everything \
+    ${MODULES} \
+    ${VIDEO_DECODERS} \
+    ${AUDIO_DECODERS} \
+    ${VIDEO_ENCODERS} \
+    ${AUDIO_ENCODERS} \
+    ${DEMUXERS} \
+    ${MUXERS} \
+    ${PARSERS}
+
+make clean
+make -j4 install
+}
+
+build_Vita
+echo Vita build finished
